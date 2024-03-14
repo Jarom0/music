41d60da206072beeb89a4967af744801a25bc5b0
Jarom0 <163330231+Jarom0@users.noreply.github.com>
2024-03-14 22:43:07 +0000

    last update 14/03/2024

--- music.py
+++ music.py
@@ -2,13 +2,6 @@ import streamlit as st
 import pandas as pd
 
 # Load data
-artists = pd.read_csv('artists_gp3.dat', delimiter='\t', names=['id', 'name', 'url', 'pictureURL'])
-user_artists = pd.read_csv('user_artists_gp3.dat', delimiter='\t', names=['userID', 'artistID', 'weight'])
-
-import streamlit as st
-import pandas as pd
-
-# Load data
 artists_df = pd.read_csv('artists_gp3.dat', delimiter='\t', names=['id', 'name', 'url', 'pictureURL'])
 user_artists_df = pd.read_csv('user_artists_gp3.dat', delimiter='\t', names=['userID', 'artistID', 'weight'])
 
@@ -41,17 +34,31 @@ def main():
     user_id = st.text_input('Enter User ID:')
     
     if st.button('Submit'):
-        try:
-            user_id = int(user_id)
-            top_artists = get_top_artists(user_id)
-            if top_artists:
-                st.write(f"Top artists for User ID {user_id}:")
-                for i, artist_name in enumerate(top_artists, start=0):
-                    st.write(f"{i}. {artist_name}")
-            else:
-                st.write("Invalid User ID or User not found.")
-        except ValueError:
-            st.write("Error: Please enter a valid User ID (integer).")
+        top_artists = get_top_artists(user_id)
+        if top_artists:
+            st.write(f"Top artists for User ID {user_id}:")
+            for i, artist_name in enumerate(top_artists, start=1):
+                st.write(f"{i}. {artist_name}")
+                # Get the picture URL for the current artist
+                artist_row = artists_df.loc[artists_df['name'] == artist_name]
+  #              picture_url = artist_row['pictureURL'].iloc[0] if not artist_row.empty else None
+   #             if picture_url:
+    #                try:
+    #                    st.image(picture_url, caption=artist_name, use_column_width=True)
+    #                except Exception as e:
+    #                    st.write(f"Error displaying image for {artist_name}: {e}")
+    #            else:
+    #                st.write(f"No picture available for {artist_name}")
+                
+                # Create a hyperlink to the artist's page
+                artist_url = artist_row['url'].iloc[0] if not artist_row.empty else None
+                if artist_url:
+                    st.markdown(f"[Click here to see the artist]({artist_url})")
+                else:
+                    st.write("No artist page available")
+        else:
+            st.write("Invalid User ID or User not found: Please enter a valid User ID (integer).")
+            st.markdown('<span style="color:red">Wait for the next update if you are a new user</span>', unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
